facff43c81eb67fdbf8d91ca75978ba01183c2ee
Edmund Yu <eyu1@ualberta.ca>
2021-08-17 23:04:18 +0000

    Use a while loop to repeat a chrous line

---
+++ chorus-while.js
@@ -0,0 +1,10 @@
+const chrous = "Lets dance!";
+let repeat = 0;
+while (repeat < 10) {
+  if (repeat === 5) {
+    console.log("*change key*");
+  }
+  console.log(chrous);
+  repeat++;
+}
+console.log("Until the sun comes up!");
